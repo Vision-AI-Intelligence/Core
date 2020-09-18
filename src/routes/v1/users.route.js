e34e8c47f066be369c87cbf11f7360bab3007b92
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-09-18 12:11:21 +0700

    added apis for user

--- src/routes/v1/users.route.js
+++ src/routes/v1/users.route.js
@@ -4,6 +4,7 @@ const admin = require("firebase-admin");
 const DataValidation = require("../../misc/DataValidation");
 const statusCode = require("../../misc/StatusCode");
 const { firestore } = require("firebase-admin");
+const similarity = require("similarity");
 router.use(authorization);
 
 /*
@@ -13,16 +14,15 @@ router.get("/", (req, res) => {
     let uid = req.user.uid;
     try {
         if (DataValidation.allNotUndefined(uid)) {
-            let getUser = admin.firestore().collection("users").doc(uid).get();
-            res.status(statusCode.OK).send({
-                user: getUser
-            });
-        } else {
             res.status(statusCode.NotFound).send({
                 message: "Not Found"
             })
             return;
         }
+        let getUser = admin.firestore().collection("users").doc(uid).get();
+        res.status(statusCode.OK).send({
+            user: getUser
+        });
     } catch (error) {
         res.status(statusCode.InternalServerError).send({
             ...error
@@ -35,30 +35,24 @@ router.get("/", (req, res) => {
 /*
 @api {POST} /v1/users Create new user profile
 */
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     let uid = req.user.uid;
     try {
-        if (DataValidation.allNotUndefined(uid)) {
-            let checkExistedUser = await admin.firestore().collection("users").doc(uid).get();
-            const userDetail = {
-                email: req.user.email,
+        let checkExistedUser = await admin.firestore().collection("users").doc(uid).get();
+        const userDetail = {
+            email: req.user.email,
 
-            };
-            if (!checkExistedUser.exists) {
-                await admin.firestore().collection("users").add({
-
-                })
-            } else {
-                res.status(statusCode.NotAcceptable).send({
-                    message: "User is existed"
-                });
-            }
-        } else {
-            res.status(statusCode.NotFound).send({
-                message: "Not Found"
+        };
+        if (checkExistedUser.exists) {
+            res.status(statusCode.NotAcceptable).send({
+                message: "User is existed"
             });
             return;
         }
+        await admin.firestore().collection("users").doc(uid).create(userDetail);
+        res.status(statusCode.OK).send({
+            message: "OK"
+        });
     } catch (error) {
         res.status(statusCode.InternalServerError).send({
             ...error
@@ -70,6 +64,33 @@ router.post("/", (req, res) => {
 /*
 @api {GET} /v1/users/listing Search the users by their info
 */
-router.get("/listing", (req, res) => { });
+router.get("/listing", async (req, res) => {
+    const { keyword } = req.body;
+    const uid = req.user.uid;
+    try {
+        if (!DataValidation.allNotUndefined(keyword)) {
+            res.status(statusCode.NotFound).send({
+                message: "Not Found"
+            })
+        }
+
+        let query = (await admin.firestore().collection("users").doc(uid).get()).data;
+        let index = similarity(keyword, query, { sensitive: true });
+        if (index < 6) {
+            res.status(statusCode.NotFound).send({
+                message: "Not Found"
+            });
+        }
+        res.status(statusCode.OK).send({
+            result: query
+        });
+
+    } catch (error) {
+        res.status(statusCode.InternalServerError).send({
+            ...error
+        });
+        console.log("GET -> listing: ", error);
+    }
+});
 
 module.exports = router;
