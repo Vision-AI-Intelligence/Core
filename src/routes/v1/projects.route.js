281a4c4946c14a7648c884e4e9c3befb2c69e5fd
Nguyễn Minh Tâm <tam.nm@itsstraining.edu.vn>
2020-09-18 10:29:09 +0700

    apis CRUD invitation

--- src/routes/v1/projects.route.js
+++ src/routes/v1/projects.route.js
@@ -3,6 +3,7 @@ const authorization = require("../../middleware/authorization");
 const admin = require("firebase-admin");
 const DataValidation = require("../../misc/DataValidation")
 const statusCode = require("../../misc/StatusCode");
+const FieldValue = admin.firestore.FieldValue;
 router.use(authorization);
 
 /*
@@ -95,7 +96,7 @@ router.post("/", async (req, res) => {
 /*
 @api {PUT} /v1/projects Update the project
 */
-router.put("/", (req, res) => {
+router.put("/", async (req, res) => {
     const { pid, name, description } = req.body;
     // Update only name and description
     // if (pid === undefined) {
@@ -159,7 +160,7 @@ router.put("/", (req, res) => {
 /*
 @api {DELETE} /v1/projects Delet the project
 */
-router.delete("/", (req, res) => {
+router.delete("/", async (req, res) => {
     const { pid } = req.query;
     // if (pid === undefined) {
     //     res.status(404).send({
@@ -204,7 +205,7 @@ router.delete("/", (req, res) => {
 /*
 @api {POST} /v1/projects/invite Create the invitation
 */
-router.post("/invite", (req, res) => {
+router.post("/invite", async (req, res) => {
     const { pid, from, to } = req.query;
     // to: not null, the collaborator's id'
     // if(pid === undefined || to === undefined){
@@ -228,6 +229,10 @@ router.post("/invite", (req, res) => {
             await admin.firestore().collection("projects").doc(pid).collection("invitation").doc().create(message);
             // send invitation to collaborator
             await admin.firestore().collection("users").doc(to).collection("invitation").doc().create(message);
+
+            res.status(statusCode.OK).send({
+                message: "OK"
+            });
         } else {
             res.status(statusCode.NotFound).send({
                 message: "Not Found"
@@ -245,11 +250,13 @@ router.post("/invite", (req, res) => {
 /*
 @api {DELETE} /v1/projects/invite Delete the invitation
 */
-router.delete("/invite", (req, res) => {
+router.delete("/invite", async (req, res) => {
     const { pid, invitationId } = req.query;
     try {
         if (DataValidation.allNotUndefined(invitationId)) {
             await admin.firestore().collection("projects").doc(pid).collection("invitation").doc(invitationId).delete();
+            // delete invitation of collaborator
+            await admin.firestore().collection("users").doc(req.user.uid).collection("invitation").doc(invitationId).delete();
             res.status(statusCode.OK).send({
                 message: "OK"
             });
@@ -270,7 +277,7 @@ router.delete("/invite", (req, res) => {
 /*
 @api {POST} /v1/projects/invite/accept Accept the invitation
 */
-router.post("/invite/accept", (req, res) => {
+router.post("/invite/accept", async (req, res) => {
     const { pid, invitationId } = req.query;
     // Get the invitation data from its id
 
@@ -279,9 +286,18 @@ router.post("/invite/accept", (req, res) => {
     try {
         if (DataValidation.allNotUndefined(invitationId)) {
             let getInvitation = await admin.firestore().collection("projects").doc(pid).collection("invitation").doc(invitationId).get();
+            let uid = req.user.uid;
             if (getInvitation.exists) {
+                // delete the invitation in project
                 await admin.firestore().collection("projects").doc(pid).collection("invitaion").doc(invitationId).delete();
-                // how to update collaborator's uid
+                // delete the invitation of user
+                await admin.firestore().collection("users").doc(uid).collection("invitation").doc(invitationId).delete();
+                await admin.firestore().collection("projects").doc(pid).set({
+                    collaborators: FieldValue.arrayUnion(uid)
+                });
+                await admin.firestore().collection("users").doc(uid).set({
+                    collaborated: FieldValue.arrayUnion(pid)
+                })
                 res.status(statusCode.OK).send({
                     message: "OK"
                 });
@@ -303,11 +319,50 @@ router.post("/invite/accept", (req, res) => {
 /*
 @api {GET} /v1/projects/collaborators Get the list of collaborators
 */
-router.get("/collaborators", (req, res) => { });
+router.get("/collaborators", async (req, res) => {
+    const { pid } = req.body;
+    try {
+        if (DataValidation.allNotUndefined(pid)) {
+            let getCollaborators = await admin.firestore().collection("projects").doc(pid).get("collaborators");
+            res.status(statusCode.OK).send({
+                collaborators: getCollaborators
+            });
+        } else {
+            res.status(statusCode.NotFound).send({
+                message: "Not Found"
+            });
+            return;
+        }
+    } catch (error) {
+        res.status(statusCode.InternalServerError).send({
+            ...error
+        });
+        console.log("GET -> collaborators: ", error);
+    }
+});
 
 /*
 @api {DELETE} /v1/projects/collaborators Remove a collaborator
 */
-router.delete("/collaborators", (req, res) => { });
+router.delete("/collaborators", async (req, res) => {
+    const { pid } = req.body;
+    let uid = req.user.uid;
+    try {
+        if (DataValidation.allNotUndefined(pid)) {
+            await admin.firestore().collection("projects").doc(pid).update({
+                collaborators: FieldValue.arrayUnion(uid)
+            });
+            res.status(statusCode.OK).send({
+                message: "OK"
+            });
+        }
+
+    } catch (error) {
+        res.status(statusCode.InternalServerError).send({
+            ...error
+        });
+        console.log("DELETE -> collaborators: ", error);
+    }
+});
 
 module.exports = router;
