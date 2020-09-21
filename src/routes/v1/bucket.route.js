7f809cf23e66cfbaf6f8d5f14de05579ce3295da
Nguyễn Tuấn Kiệt <nguyentuankiet23031998@gmail.com>
2020-09-21 10:54:17 +0700

    Add more bucket functions

--- src/routes/v1/bucket.route.js
+++ src/routes/v1/bucket.route.js
@@ -45,8 +45,14 @@ router.get("/metadata", async (req, res) => {
 
 /**
  * @api {POST} /v1/bucket/upload Upload a file
+ * @apiParam  {String} pid Project's id
+ * @apiParam  {String} bid Bucket's id
+ * @apiParam  {String} d Current directory. Default is root /
  */
-router.post("/upload", async (req, res) => {});
+router.post("/upload", async (req, res) => {
+  const { pid, bid, d } = req.query;
+  // Checksum is optional
+});
 
 /**
  * @api {PUT} /v1/bucket/mkdir Make new directory
@@ -101,7 +107,25 @@ router.get("/download", async (req, res) => {
   const { pid, bid, f } = req.query;
 });
 
-// Zip, Unzip are the worker jobs
+// Zip, Unzip, Download-job are the worker jobs
+
+/**
+ * @api {GET} /v1/bucket/jobs Get the list of jobs
+ * @apiParam  {String} pid Project's id
+ * @apiSuccessExample {JSON} Success-Response:
+ *    {
+ *      "job-01": {
+ *          "type": "unzip",
+ *          "status": "200",
+ *          "details": "...."
+ *       },
+ *      "job-02":{...},
+ *      ...
+ *    }
+ */
+router.get("/jobs", async (req, res) => {
+  const { pid } = req.params;
+});
 
 /**
  * @api {PUT} /v1/bucket/zip Zip files and folders
@@ -123,6 +147,17 @@ router.put("/zip", async (req, res) => {
  */
 router.put("/unzip", async (req, res) => {
   const { pid, bid, src, des } = req.body;
+});
+
+/**
+ * @api {PUT} /v1/bucket/download-job Create a download job
+ * @apiParam  {String} pid Project's id
+ * @apiParam  {String} bid Bucket's id
+ * @apiParam  {String} url The url of the resource
+ * @apiParam  {String} des Destination file|directory
+ */
+router.put("/download-job", async (req, res) => {
+  const { pid, bid, url, des } = req.body;
 });
 
 module.exports = router;
